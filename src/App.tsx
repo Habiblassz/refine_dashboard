b94b2c98df480176e1b359f9ab2aeca4dc178993
Lawal Habib <habiblass2001@gmail.com>
2025-03-19 12:10:57 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,91 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+import { authProvider, dataProvider, liveProvider } from "./providers";
 import routerBindings, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
+	CatchAllNavigate,
+	DocumentTitleHandler,
+	UnsavedChangesNotifier,
 } from "@refinedev/react-router";
 import { App as AntdApp } from "antd";
-import { createClient } from "graphql-ws";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { authProvider } from "./authProvider";
-import { ColorModeContextProvider } from "./contexts/color-mode";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import { CompanyList, ForgotPassword, Home, Login, Register } from "./pages";
+import Layout from "./components/layout";
+import { resources } from "./config/resources";
+import Create from "./pages/comapany/create";
+import EditPage from "./pages/comapany/edit";
+import List from "./pages/tasks/list";
 
-const API_URL = "https://api.nestjs-query.refine.dev/graphql";
-const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
-
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
+import EditTask from "./pages/tasks/edit";
+import CreateTask from "./pages/tasks/create";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <AntdApp>
-            <DevtoolsProvider>
-              <Refine
-                dataProvider={dataProvider(gqlClient)}
-                liveProvider={liveProvider(wsClient)}
-                notificationProvider={useNotificationProvider}
-                routerProvider={routerBindings}
-                authProvider={authProvider}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  useNewQueryKeys: true,
-                  projectId: "VmvwF2-uA8Y7H-7iZ2TF",
-                  liveMode: "auto",
-                }}
-              >
-                <Routes>
-                  <Route index element={<WelcomePage />} />
-                </Routes>
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-              </Refine>
-              <DevtoolsPanel />
-            </DevtoolsProvider>
-          </AntdApp>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<GitHubBanner />
+			<RefineKbarProvider>
+				<AntdApp>
+					<DevtoolsProvider>
+						<Refine
+							dataProvider={dataProvider}
+							liveProvider={liveProvider}
+							notificationProvider={useNotificationProvider}
+							routerProvider={routerBindings}
+							authProvider={authProvider}
+							resources={resources}
+							options={{
+								syncWithLocation: true,
+								warnWhenUnsavedChanges: true,
+								useNewQueryKeys: true,
+								projectId: "VmvwF2-uA8Y7H-7iZ2TF",
+								liveMode: "auto",
+							}}>
+							<Routes>
+								<Route path="/register" element={<Register />} />
+								<Route path="/login" element={<Login />} />
+								<Route path="/forgot-password" element={<ForgotPassword />} />
+								<Route
+									element={
+										<Authenticated
+											key="authenticated-layout"
+											fallback={<CatchAllNavigate to="/login" />}>
+											<Layout>
+												<Outlet />
+											</Layout>
+										</Authenticated>
+									}>
+									<Route index element={<Home />} />
+									<Route path="/companies">
+										<Route index element={<CompanyList />} />
+										<Route path="new" element={<Create />} />
+										<Route path="edit/:id" element={<EditPage />} />
+									</Route>
+									<Route
+										path="/tasks"
+										element={
+											<List>
+												<Outlet />
+											</List>
+										}>
+										<Route path="new" element={<CreateTask />}></Route>
+										<Route path="edit/:id" element={<EditTask />}></Route>
+									</Route>
+								</Route>
+							</Routes>
+							<RefineKbar />
+							<UnsavedChangesNotifier />
+							<DocumentTitleHandler />
+						</Refine>
+						<DevtoolsPanel />
+					</DevtoolsProvider>
+				</AntdApp>
+			</RefineKbarProvider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export * from "./home";
+export * from "./login";
+export * from "./register";
+export * from "./forgotPassword";
+export * from "./comapany/list";
+export * from "./comapany/create";
+export * from "./comapany/edit";
